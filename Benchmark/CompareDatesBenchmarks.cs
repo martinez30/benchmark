6051456fc75581cc2f8dfead708e6dda58e23dea
Paulo <paulosm.dev@gmail.com>
2023-11-13 11:53:48 -0300

    compare dates

--- Benchmark/CompareDatesBenchmarks.cs
+++ Benchmark/CompareDatesBenchmarks.cs
@@ -11,28 +11,46 @@ public class CompareDatesBenchmarks
     [Benchmark]
     public bool CompareWithAcessors()
     {
-        return dateV1.Date == dateV2.Date && dateV1.Month == dateV2.Month && dateV1.Year == dateV2.Year;
+        return dateV1.Day == dateV2.Day && dateV1.Month == dateV2.Month && dateV1.Year == dateV2.Year;
+    }
+
+    [Benchmark]
+    public bool CompareWithAcessorsAndEquals()
+    {
+        return dateV2.Day.Equals(dateV2.Day) && dateV1.Month.Equals(dateV2.Month) && dateV1.Year.Equals(dateV2.Year);
     }
     
     [Benchmark]
+    public bool CompareWithAcessorsDate()
+    {
+        return dateV1.Date == dateV2.Date;
+    }
+
+    [Benchmark]
+    public bool CompareWithAcessorsAndEqualsDate()
+    {
+        return dateV2.Date.Equals(dateV2.Date);
+    }
+    
+    // [Benchmark]
     public bool CompareWithOperatorString()
     {
         return dateV1.ToString("yyyy-MM-dd") == dateV2.ToString("yyyy-MM-dd");
     }
     
-    [Benchmark]
+    // [Benchmark]
     public bool CompareWithOperatorStringAndCulture()
     {
         return dateV1.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) == dateV2.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture);
     }
 
-    [Benchmark]
+    // [Benchmark]
     public bool CompareWithShortDateString()
     {
         return dateV1.ToShortDateString() == dateV2.ToShortDateString();
     }
 
-    [Benchmark]
+    // [Benchmark]
     public bool CompareWithShortDateStringAndEqual()
     {
         return dateV1.ToShortDateString().Equals(dateV2.ToShortDateString());
